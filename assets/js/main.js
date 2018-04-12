998b69753a0dd4463b5576e0a141e9a2c04c3b0e
capJavert <ante.baric3@gmail.com>
2018-04-12 18:32:47 +0200

    beerup v0.0.1

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+
+function toggleMenu() {
+  let button = document.querySelector(".nav-button");
+  let navigation = document.querySelector(".main-nav");
+
+  if (navigation.className.includes("toggle-on")) {
+    button.innerHTML = "<i class=\"icon-menu\"></i>";
+    navigation.className = "navigation main-nav";
+  } else {
+    button.innerHTML = "<i class=\"icon-cancel\"></i>";
+
+    navigation.animate([
+      { opacity: 0 },
+      { opacity: 1 }
+    ], 400);
+    navigation.className = "navigation main-nav toggle-on";
+  }
+}
+
+window.onscroll = () => {
+  toggleFixedBeerCrate()
+};
+
+let fixedOffset = null;
+
+/**
+ * Listener for fixed beer crate sidebar when scrolling
+ */
+function toggleFixedBeerCrate() {
+  let beerCrateSidebar = document.getElementById("beer-crate-sidebar");
+
+  if (beerCrateSidebar.className !== 'fixed') {
+    fixedOffset = beerCrateSidebar.offsetTop;
+  }
+
+  if (window.pageYOffset >= fixedOffset + 100) { // added 50px offset so the page does not jump
+    beerCrateSidebar.classList.add("fixed");
+  } else {
+    beerCrateSidebar.classList.remove("fixed");
+  }
+}
